f26e5c2bfc441578d7e79bf065b247b388f8a899
VBulat74 <Vladimir.Bulat@gmail.com>
2024-01-13 20:21:48 +0300

    Lesson_06

--- app/src/main/java/ru/com/bulat/courutineflowsoumin/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/ru/com/bulat/courutineflowsoumin/crypto_app/CryptoViewModel.kt
@@ -1,13 +1,12 @@
 package ru.com.bulat.courutineflowsoumin.crypto_app
 
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 
@@ -17,9 +16,16 @@ class CryptoViewModel : ViewModel() {
 
     val state: LiveData<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
-        .map { State.Content(currencyList = it) as State}
+        .map { State.Content(currencyList = it) as State }
         .onStart {
+            Log.d("AAA", "onStart")
             emit(State.Loading)
+        }
+        .onEach {
+            Log.d("AAA", "onEach")
+        }
+        .onCompletion {cause: Throwable? ->
+            Log.d("AAA", "onCompletion")
         }
         .asLiveData()
 }
