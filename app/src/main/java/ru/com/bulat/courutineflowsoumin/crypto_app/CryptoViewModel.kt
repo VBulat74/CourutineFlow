88ffe8292bae3825cbe3fbe207c8f929e88e583f
VB <Vladimir.Bulat@gmail.com>
2024-01-16 09:15:48 +0300

    Lesson_11

--- app/src/main/java/ru/com/bulat/courutineflowsoumin/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/ru/com/bulat/courutineflowsoumin/crypto_app/CryptoViewModel.kt
@@ -3,21 +3,31 @@ package ru.com.bulat.courutineflowsoumin.crypto_app
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
     private val repository = CryptoRepository
 
-    val state: Flow<State> = repository.getCurrencyList()
+    private val loadingFlow = MutableSharedFlow<State>()
+
+    val state : Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
-        .onStart {emit(State.Loading)}
+        .onStart { emit(State.Loading) }
+        .mergeWith(loadingFlow)
+
+    private fun <T> Flow<T>.mergeWith(another : Flow<T>) : Flow<T> {
+        return listOf(this, another).merge()
+    }
 
     fun refreshList() {
         viewModelScope.launch {
+            loadingFlow.emit(State.Loading)
             repository.refreshList()
         }
     }
